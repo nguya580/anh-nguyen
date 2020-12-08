694b4ecb807dca463cebd087138dd00533f665c3
Anh Nguyen <nguya580@newschool.edu>
2020-12-08 14:46:50 -0500

    update export cvs

--- nounproject_icons_sort_countries/scrapbook.py
+++ nounproject_icons_sort_countries/scrapbook.py
@@ -10,12 +10,14 @@ from requests_oauthlib import OAuth1
 
 # %%
 
-auth = OAuth1("**********", "**********")
+auth = OAuth1("Insert-Key", "Insert-Secret")
 endpoint = "http://api.thenounproject.com/icons/food?&limit=50"
 
 response = requests.get(endpoint, auth=auth)
 
 data = response.content
+
+print(data)
 
 # %%
 
@@ -31,6 +33,7 @@ for icon in data['icons']:
 uploaders
 
 # %%
+
 df = pd.DataFrame(uploaders)
 
 pd.set_option('display.max_rows', 60)
@@ -50,43 +53,43 @@ df
 # %% sort countries column
 
 #if need extract first 2 chars from each user
-df['country code'] = df.location.str[-2:]
-df.sort_values(by='country code', ascending=True, inplace=True)
+df['country_code'] = df.location.str[-2:]
+df.sort_values(by='country_code', ascending=True, inplace=True)
 df.reset_index(inplace=True, drop=True)
 
-df['country code'].value_counts()
+df['country_code'].value_counts()
 
 # %%
 # create a list of our conditions
 conditions = [
-    (df['country code'] == 'GB'),
-    (df['country code'] == 'US'),
-    (df['country code'] == 'FR'),
-    (df['country code'] == 'IN'),
-    (df['country code'] == 'PK'),
-    (df['country code'] == 'MX'),
-    (df['country code'] == 'RU'),
-    (df['country code'] == 'CH'),
-    (df['country code'] == 'EG'),
-    (df['country code'] == 'TH'),
-    (df['country code'] == 'IR'),
-    (df['country code'] == 'CA'),
-    (df['country code'] == 'TR'),
-    (df['country code'] == 'BR'),
-    (df['country code'] == 'ES'),
-    (df['country code'] == 'DE'),
-    (df['country code'] == 'AU'),
-    (df['country code'] == 'IT'),
-    (df['country code'] == 'BG'),
+    (df['country_code'] == 'GB'),
+    (df['country_code'] == 'US'),
+    (df['country_code'] == 'FR'),
+    (df['country_code'] == 'IN'),
+    (df['country_code'] == 'PK'),
+    (df['country_code'] == 'MX'),
+    (df['country_code'] == 'RU'),
+    (df['country_code'] == 'CH'),
+    (df['country_code'] == 'EG'),
+    (df['country_code'] == 'TH'),
+    (df['country_code'] == 'IR'),
+    (df['country_code'] == 'CA'),
+    (df['country_code'] == 'TR'),
+    (df['country_code'] == 'BR'),
+    (df['country_code'] == 'ES'),
+    (df['country_code'] == 'DE'),
+    (df['country_code'] == 'AU'),
+    (df['country_code'] == 'IT'),
+    (df['country_code'] == 'BG'),
     ]
 
 # create a list of the values we want to assign for each condition
 values = ['🇬🇧', '🇺🇸', '🇫🇷', '🇮🇳', '🇵🇰', '🇲🇽', '🇷🇺', '🇨🇭', '🇪🇬', '🇹🇭', '🇮🇷', '🇨🇦', '🇹🇷', '🇧🇷', '🇪🇸', '🇩🇪', '🇦🇺', '🇮🇹', '🇧🇬']
 
-df['country emoji'] = np.select(conditions, values)
+df['country_emoji'] = np.select(conditions, values)
 
 df
 
 # %% write json
 
-df.to_json('nounproject_countries.json', orient='records', lines=True)
+df.to_csv('nounproject_countries.csv')
